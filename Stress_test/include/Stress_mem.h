5d0b4b92e28040ce47706d13715e6ef6a9ca64dd
nxt9 <bouh.neo@gmail.com>
2016-02-12 15:31:59 +0100

    Debut de l'implémentation de stress_mem

---
+++ Stress_test/include/Stress_mem.h
@@ -0,0 +1,16 @@
+#ifndef STRESS_MEM_H
+#define STRESS_MEM_H
+
+#include <Stress.h>
+
+
+class Stress_mem : public Stress
+{
+    public:
+        Stress_mem();
+        virtual ~Stress_mem();
+    protected:
+    private:
+};
+
+#endif // STRESS_MEM_H
---
+++ Stress_test/src/Stress_mem.cpp
@@ -0,0 +1,11 @@
+#include "Stress_mem.h"
+
+Stress_mem::Stress_mem()
+{
+    //ctor
+}
+
+Stress_mem::~Stress_mem()
+{
+    //dtor
+}
